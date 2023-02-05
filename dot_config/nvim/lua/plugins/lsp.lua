b671f4a3f1b443461f5862b0c56717441b53a888
Brandon Hops <hops.brandon@gmail.com>
2023-02-05 09:45:50 -0500

    LSP updates

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,23 +1,92 @@
 return {
   {
     "simrat39/rust-tools.nvim",
-    opts = {},
-  },
-  {
-    "neovim/nvim-lspconfig",
     opts = {
-      servers = {
-        gopls = {},
+      server = {
+        on_attach = function(_, bufnr)
+          local rt = require("rust-tools")
+          -- Hover actions
+          vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+          -- Code action groups
+          vim.keymap.set("n", "<leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+
+          rt.inlay_hints.enable()
+        end,
       },
     },
   },
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "simrat39/inlay-hints.nvim",
+    },
+    opts = {
+      servers = {
+        bashls = {},
+        dockerls = {},
+        gopls = {
+          on_attach = function(c, b)
+            local ih = require("inlay-hints")
+            ih.on_attach(c, b)
+          end,
+          settings = {
+            gopls = {
+              hints = {
+                constantValues = true,
+              },
+            },
+          },
+        },
+        jsonls = {},
+        yamlls = {},
+        sumneko_lua = {
+          single_file_support = true,
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = {
+                workspaceWord = true,
+                callSnippet = "Both",
+              },
+            },
+          },
+        },
+        vimls = {},
+      },
+    },
+  },
+
   {
     "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "rust-analyzer",
-        "gopls",
-      })
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "luacheck",
+        "shellcheck",
+        "shfmt",
+        "black",
+        "flake8",
+        "isort",
+      },
+    },
+  },
+
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          -- nls.builtins.formatting.prettierd,
+          nls.builtins.formatting.stylua,
+          nls.builtins.diagnostics.flake8,
+          nls.builtins.formatting.isort,
+          nls.builtins.formatting.black,
+        },
+      }
     end,
   },
 }
