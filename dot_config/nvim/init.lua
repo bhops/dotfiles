1b9c1e5870ad9de1abb52e82c6fa06424acc467b
Brandon Hops <hops.brandon@gmail.com>
2023-11-25 16:06:14 -0500

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -49,6 +49,49 @@ require("lazy").setup({
 		},
 	},
 	{
+		"rest-nvim/rest.nvim",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+		config = function()
+			require("rest-nvim").setup({
+				-- Open request results in a horizontal split
+				result_split_horizontal = false,
+				-- Keep the http file buffer above|left when split horizontal|vertical
+				result_split_in_place = false,
+				-- Skip SSL verification, useful for unknown certificates
+				skip_ssl_verification = false,
+				-- Encode URL before making request
+				encode_url = true,
+				-- Highlight request on run
+				highlight = {
+					enabled = true,
+					timeout = 150,
+				},
+				result = {
+					-- toggle showing URL, HTTP info, headers at top the of result window
+					show_url = true,
+					-- show the generated curl command in case you want to launch
+					-- the same request via the terminal (can be verbose)
+					show_curl_command = false,
+					show_http_info = true,
+					show_headers = true,
+					-- executables or functions for formatting response body [optional]
+					-- set them to false if you want to disable them
+					formatters = {
+						json = "jq",
+						html = function(body)
+							return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+						end,
+					},
+				},
+				-- Jump to request line on run
+				jump_to_request = false,
+				env_file = ".env",
+				custom_dynamic_variables = {},
+				yank_dry_run = true,
+			})
+		end,
+	},
+	{
 		"nvim-tree/nvim-tree.lua",
 		config = function()
 			require("nvim-tree").setup({
@@ -246,7 +289,7 @@ vim.o.sidescrolloff = 8
 vim.o.tabstop = 2
 
 require("nvim-treesitter.configs").setup({
-	ensure_installed = { "go", "lua", "nix", "rust", "vim" },
+	ensure_installed = { "go", "lua", "rust", "vim", "http", "json" },
 	highlight = { enable = true },
 	indent = { enable = false },
 })
