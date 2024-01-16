f0db8e753d9acb061aae30094fec47eebdad3511
Brandon Hops <hops.brandon@gmail.com>
2024-01-16 11:56:04 -0500

    Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -59,61 +59,6 @@ require("lazy").setup({
 		end,
 	},
 	{
-		"rest-nvim/rest.nvim",
-		dependencies = { { "nvim-lua/plenary.nvim" } },
-		keys = {
-			{
-				"<leader>rr",
-				function()
-					require("rest-nvim").run()
-				end,
-				mode = "",
-				desc = "REST Request",
-			},
-		},
-		config = function()
-			require("rest-nvim").setup({
-				-- Don't move focus to the split
-				stay_in_current_window_after_split = true,
-				-- Open request results in a horizontal split
-				result_split_horizontal = true,
-				-- Keep the http file buffer above|left when split horizontal|vertical
-				result_split_in_place = true,
-				-- Skip SSL verification, useful for unknown certificates
-				skip_ssl_verification = false,
-				-- Encode URL before making request
-				encode_url = true,
-				-- Highlight request on run
-				highlight = {
-					enabled = true,
-					timeout = 150,
-				},
-				result = {
-					-- toggle showing URL, HTTP info, headers at top the of result window
-					show_url = true,
-					-- show the generated curl command in case you want to launch
-					-- the same request via the terminal (can be verbose)
-					show_curl_command = true,
-					show_http_info = true,
-					show_headers = true,
-					-- executables or functions for formatting response body [optional]
-					-- set them to false if you want to disable them
-					formatters = {
-						json = "jq",
-						html = function(body)
-							return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
-						end,
-					},
-				},
-				-- Jump to request line on run
-				jump_to_request = false,
-				env_file = ".env",
-				custom_dynamic_variables = {},
-				yank_dry_run = true,
-			})
-		end,
-	},
-	{
 		"nvim-tree/nvim-tree.lua",
 		config = function()
 			require("nvim-tree").setup({
@@ -193,7 +138,7 @@ require("lazy").setup({
 		opts = {
 			options = {
 				icons_enabled = false,
-				theme = "rose-pine",
+				theme = "everforest",
 				component_separators = "|",
 				section_separators = "",
 			},
@@ -277,14 +222,22 @@ require("lazy").setup({
 		},
 	},
 	{
-		"rose-pine/neovim",
-		name = "rose-pine",
+		"sainnhe/everforest",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("rose-pine")
+			vim.cmd.colorscheme("everforest")
 		end,
 	},
+	--{
+	--	"rose-pine/neovim",
+	--	name = "rose-pine",
+	--	lazy = false,
+	--	priority = 1000,
+	--	config = function()
+	--		vim.cmd.colorscheme("rose-pine")
+	--	end,
+	--},
 	{
 		"nvim-treesitter/nvim-treesitter",
 		dependencies = {
