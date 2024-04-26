04b4d223ba8a856132c36ab10777dbf1127a0ff6
Brandon Hops <hops.brandon@gmail.com>
2024-04-26 18:43:51 -0400

    Pin bufferline due to error https://github.com/akinsho/bufferline.nvim/issues/895

--- home/private_dot_config/nvim/lua/bhops/plugins/bufferline.lua
+++ home/private_dot_config/nvim/lua/bhops/plugins/bufferline.lua
@@ -1,5 +1,8 @@
 return {
 	"akinsho/bufferline.nvim",
+	branch = "main",
+	commit = "f6f00d9ac1a51483ac78418f9e63126119a70709",
+	-- version = "*", -- https://github.com/akinsho/bufferline.nvim/issues/895
 	event = "VeryLazy",
 	opts = {
 		options = {
